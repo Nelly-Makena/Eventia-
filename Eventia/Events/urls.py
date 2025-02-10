b5061756ab261b9154de987351e511930abef8d0
NellyMakena <nellysolomonmakena3@gmail.com>
2025-02-10 13:26:04 +0300

    login_out functionality

--- Eventia/Events/urls.py
+++ Eventia/Events/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home_view,register_view,login_view,pricing_view,subscription_view,submit_subscription_view,events_view,help_view
+from .views import home_view,register_view,login_view,pricing_view,subscription_view,submit_subscription_view,events_view,help_view,logout_view
 # Import your views module
 
 urlpatterns = [
@@ -11,6 +11,7 @@ urlpatterns = [
     path('subscription/',subscription_view,name='subscription'),
     path('subscription/submit/', submit_subscription_view, name='submit_subscription'),
     path('help/',help_view,name='help'),
+    path('logout/',logout_view,name='logout'),
 
 
 ]
